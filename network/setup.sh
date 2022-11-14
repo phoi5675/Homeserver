4d2e79fea58594bf58ede39e0f870e6297823f56
phoi5675 <phoi5675@gmail.com>
2022-11-14 22:59:18 +0900

    change firewall from iptables to ufw

--- network/setup.sh
+++ network/setup.sh
@@ -2,8 +2,8 @@
 # TODO: Add script for date / time settings.
 
 # Edit interface name based on network interfaces
-readonly INB_IFACE=eth0  # Inbound
-readonly OUTB_IFACE=eth1 # Outbound
+readonly WAN_IFACE=eth0 # Inbound
+readonly LAN_IFACE=eth1 # Outbound
 
 networkd="systemd-networkd.service"
 
@@ -20,39 +20,30 @@ edit_interface() {
 auto lo
 iface lo inet loopback
 
-# Outbound
-auto ${OUTB_IFACE}
-iface ${OUTB_IFACE} inet dhcp
-
-# Inbound
-auto ${INB_IFACE}
-iface ${INB_IFACE} inet static
+# LAN
+auto ${WAN_IFACE}
+iface ${WAN_IFACE} inet static
 address 10.10.10.1
 netmask 255.255.255.0
+
+# WAN 
+auto ${LAN_IFACE}
+iface ${LAN_IFACE} inet dhcp
 
 # Private gateway for VM and CT
 
 # Router setting
 post-up   echo 1 > /proc/sys/net/ipv4/ip_forward
-post-up   iptables -t nat -A POSTROUTING -s '10.10.10.0/24' -o ${OUTB_IFACE} -j MASQUERADE
-post-down iptables -t nat -D POSTROUTING -s '10.10.10.0/24' -o ${OUTB_IFACE} -j MASQUERADE
-post-up iptables -t nat -A PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8000 -j DNAT --to 10.10.10.1:80
-post-down iptables -t nat -D PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8000 -j DNAT --to 10.10.10.1:80
-post-up iptables -t nat -A PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8443 -j DNAT --to 10.10.10.1:443
-post-down iptables -t nat -D PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8443 -j DNAT --to 10.10.10.1:443
-# For tcp proxy streaming
-post-up iptables -t nat -A PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8001 -j DNAT --to 10.10.10.1:8001
-post-down iptables -t nat -D PREROUTING -i ${OUTB_IFACE} -p tcp --dport 8001 -j DNAT --to 10.10.10.1:8001
 EOF
 
     echo "Restarting service..."
     sudo systemctl restart "${networkd}"
     echo "Restarting interfaces..."
-    sudo ifconfig "${INB_IFACE}" down
-    sudo ifconfig "${INB_IFACE}" up
+    sudo ifconfig "${WAN_IFACE}" down
+    sudo ifconfig "${WAN_IFACE}" up
 
-    sudo ifconfig "${OUTB_IFACE}" down
-    sudo ifconfig "${OUTB_IFACE}" up
+    sudo ifconfig "${LAN_IFACE}" down
+    sudo ifconfig "${LAN_IFACE}" up
 
 }
 
@@ -87,6 +78,10 @@ EOF
 
     echo "Restart nginx service..."
     sudo service nginx restart
+}
+
+set_ufw() {
+    echo "Set ufw configs..."
 }
 
 set_host() {
