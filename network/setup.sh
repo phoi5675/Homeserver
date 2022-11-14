bcf30b1dbf2c85f8f848fedb9ea85d324331ce1a
phoi5675 <phoi5675@gmail.com>
2022-11-15 00:13:53 +0900

    fixing ufw config...

--- network/setup.sh
+++ network/setup.sh
@@ -5,12 +5,14 @@
 readonly WAN_IFACE=eth0 # Inbound
 readonly LAN_IFACE=eth1 # Outbound
 
+readonly LOC_NAT="10.10.10.0/24"
+
 networkd="systemd-networkd.service"
 
 edit_interface() {
     local interfaces="/etc/network/interfaces"
     echo "Install net-tools"
-    sudo apt-get --quiet update && sudo apt-get --quiet install net-tools -y
+    sudo apt-get -qq update && sudo apt-get --quiet install net-tools -y
     echo "Backup ${interfaces}..."
     sudo mv "${interfaces}" "${interfaces}.bak"
     echo "Edit ${interfaces}..."
@@ -31,9 +33,6 @@ auto ${LAN_IFACE}
 iface ${LAN_IFACE} inet dhcp
 
 # Private gateway for VM and CT
-
-# Router setting
-post-up   echo 1 > /proc/sys/net/ipv4/ip_forward
 EOF
 
     echo "Restarting service..."
@@ -53,8 +52,8 @@ set_nginx() {
     local stream_conf_dir=${nginx_dir}"/stream"
     local sites_available=${nginx_dir}"/sites-available"
     echo "Install nginx..."
-    sudo apt-get --quiet update
-    sudo apt-get --quiet install nginx -y
+    sudo apt-get -qq update
+    sudo apt-get -qq install nginx -y
 
     echo "Setup nginx..."
     # echo "Delete configs in ${sites_available}..."
@@ -81,7 +80,30 @@ EOF
 }
 
 set_ufw() {
+    local default_config="/etc/default/ufw"
+    local ufw_sysctl="/etc/ufw/sysctl.conf"
+    local ufw_before="/etc/ufw/before.rules"
+
+    echo "Install ufw..."
+    sudo apt-get update -qq && sudo apt-get install ufw -y -qq
+
+    echo "Enable port forwarding..."
+    sudo sed -i '/net\/ipv4\/ip_forward/s/^#//g' "${ufw_sysctl}"
     echo "Set ufw configs..."
+    sudo sed -i 's/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g' "${default_config}"
+
+    echo "Set MASQUERADE..."
+    local insert_pos="$(($(cat \/etc\/ufw\/before.rules | wc -l) - 3))"
+    # TODO: 일단 ufw 그냥 파일로 때워버릴지 말지 결정하기,,,
+    local before_rules="\n
+# NAT Table rules \n
+\*nat \n
+:POSTROUTING ACCEPT \[0:0] \n
+\n
+# Forward traffic from ${LOC_NAT} to ${WAN_IFACE} \n
+-A POSTROUTING -s ${LOC_NAT} -o ${WAN_IFACE} -j MASQUERADE \n
+"
+    sudo sed -i "${insert_pos}i ${before_rules}" "${ufw_before}"
 }
 
 set_host() {
@@ -94,11 +116,12 @@ set_host() {
 
 set_apt_mirror() {
     sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
-    sudo apt-get -q update
+    sudo apt-get -qq update
 }
 
 main() {
     set_apt_mirror
+    set_ufw
     edit_interface
     set_host
     set_nginx
