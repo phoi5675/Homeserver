eeb3b047ee9ff86fbc20e5dd3a4b5cd172a318a3
phoi5675 <phoi5675@gmail.com>
2022-11-16 17:32:14 +0900

    add TLS setup

--- network/setup.sh
+++ network/setup.sh
@@ -123,6 +123,19 @@ COMMIT
     sudo ufw disable && sudo ufw enable
 }
 
+set_certbot() {
+    echo "Get TLS certificate..."
+    echo "Installing certbot..."
+    sudo apt-get update -qq &&
+        sudo apt-get install certbot python3-certbot-nginx -y -qq
+    echo "Issuing certificates..."
+    echo "Add values to DNS Server MANUALLY!"
+    sudo certbot certonly --manual --preferred-challenges \
+        dns -d "*.phoiweb.com" -d "phoiweb.com"
+    # TODO: Add auto-renewal script
+    # 0 0 1 * * /bin/bash -l -c 'certbot renew --quiet'
+}
+
 set_host() {
     local hosts="/etc/hosts"
     echo "Set ${hosts}..."
