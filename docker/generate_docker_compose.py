35ecad427f8326a27e1b9505e03e5cbedb0456d3
phoi5675 <phoi5675@gmail.com>
2022-12-26 16:29:49 +0900

    Update docString for composers

--- docker/generate_docker_compose.py
+++ docker/generate_docker_compose.py
@@ -1,6 +1,10 @@
 
 def read_base_docker_compose() -> None:
+    """Create object from yml file
+    """    
     pass
 
 def generate_docker_compose() -> None:
+    """Generate docker-compose.yml from "config" files
+    """    
     pass
--- docker/get_ip.py
+++ docker/get_ip.py
@@ -1,7 +1,11 @@
 import sys
 
 
-def process_hostfile() -> None:
-    """_summary_
+def process_hostfile(host: str, ip: str) -> None:
+    """Insert host and ip from hosts to docker-compose.yml
+
+    Args:
+        host (str): hostname
+        ip (str): ip address of host
     """
     pass
