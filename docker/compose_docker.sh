7a35ff3e1cc55ff5532ec681fe18d6f4e67aacff
phoi5675 <phoi5675@gmail.com>
2022-12-27 14:48:44 +0900

    add docker-compose.yml

--- docker/compose_docker.sh
+++ docker/compose_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3 host_to_env.py --host_path='../router/setups/hosts'
+python3 host_to_env.py --host_path='../router/setups/hosts' --env_path='./env/.env'
 
 sudo docker-compose $(find ./ -name docker*.yml | sed -e 's/^/-f /') up -d
